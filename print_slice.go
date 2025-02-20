fa956a828e7e761483d015bfa31480b0bae1340d
Jeremiamichael <jeremia.mikhael@binus.ac.id>
2025-02-20 15:28:21 +0700

    Updated

--- print_slice.go
+++ print_slice.go
@@ -2,10 +2,16 @@ package print_slice
 
 import "fmt"
 
-func PrintArray2D(array [][]int) {
-	for _, rows := range array {
-		for _, cols := range rows {
-			fmt.Printf("%d ", cols)
+func PrintSlice(slice []int) {
+	for i := 0; i < len(slice); i++ {
+		fmt.Print(slice[i], " ")
+	}
+}
+
+func PrintSlice2D(slice [][]int) {
+	for i := 0; i < len(slice); i++ {
+		for j := 0; j < len(slice[i]); j++ {
+			fmt.Print(slice[i][j], "\t")
 		}
 		fmt.Println()
 	}
